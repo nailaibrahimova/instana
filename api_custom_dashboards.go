bd1c8f2bdb90be584955faa2cb4ea0b080f88341
ngibrahimova <naila.ibrahimova@pashabank.az>
2021-08-11 15:16:17 +0400

    Remove logs of request

--- api_custom_dashboards.go
+++ api_custom_dashboards.go
@@ -13,7 +13,6 @@ package instana
 import (
 	_context "context"
 	"github.com/antihax/optional"
-	"github.com/google/martian/log"
 	_ioutil "io/ioutil"
 	_nethttp "net/http"
 	_neturl "net/url"
@@ -365,11 +364,6 @@ func (a *CustomDashboardsApiService) GetCustomDashboards(ctx _context.Context) (
 	if err != nil {
 		return localVarReturnValue, nil, err
 	}
-
-	log.Infof("Dashboard request header: %+v", r.Header)
-	log.Infof("Dashboard request body: %+v", r.Body)
-	log.Infof("Dashboard request method: %+v", r.Method)
-	log.Infof("Dashboard request url: %+v", r.URL)
 
 	localVarHTTPResponse, err := a.client.callAPI(r)
 	if err != nil || localVarHTTPResponse == nil {
